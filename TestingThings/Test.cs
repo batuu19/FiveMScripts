4661ff9f402e4e4e6524d2f4a297534222aa396a
Bartek <bartek.blazkow@o2.pl>
2018-11-13 21:34:23 +0100

    API.RegisterCommand some cleaning

--- TestingThings/Test.cs
+++ TestingThings/Test.cs
@@ -17,41 +17,30 @@ namespace TestingThings
 
         public Test()
         {
+            API.RegisterCommand("jumper", new Action<int, List<object>, string>(ToggleJumper), false);
+            API.RegisterCommand("spawn", new Action<int, List<object>, string>(SpawnCar), false);
+            API.RegisterCommand("wanted", new Action<int, List<object>, string>(SetWantedLevel), false);
             Tick += OnTick;
-            try
-            {
-                EventHandlers["chatMessage"] += new Action<string, List<object>, string>(OnChatMessage);
-                Debug.WriteLine("Registered Command");
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine($"Error in jumper constructor: {e}");
-            }
-            //not working here
-            //Game.PlayerPed.IsCollisionProof = true;
+
         }
 
-
+        #region OnTick
         private async Task OnTick()
         {
             try
             {
                 var player = Game.PlayerPed;
-                if(Game.IsControlJustPressed(0,Control.Jump) && jumper)
+                if (Game.IsControlJustPressed(0, Control.Jump) && jumper)
                 {
-                    player.ApplyForce(new Vector3(0,0,forceMultipier));
+                    player.ApplyForce(new Vector3(0, 0, forceMultipier));
                     DrawNotification("Force added");
                 }
 
-                if(player.IsRagdoll)
+                if (player.IsRagdoll)
                 {
                     player.CancelRagdoll();
                 }
-
                 player.IsCollisionProof = true;
-
-                TriggerEvent("chatMessage", "", new int[] { 0, 0, 0 }, "Hello world");
-                //cały czas pisze to, to źle
             }
             catch (Exception e)
             {
@@ -59,75 +48,57 @@ namespace TestingThings
             }
 
         }
-
-        private void OnChatMessage(string author, List<object> color, string text)
-        {
-            try
-            {
-                if (text[0] == '/')
-                    ExecuteCommand(text.Substring(1));
-            }
-            catch (Exception e)
-            {
-                DrawNotification($"Command error: {e}");
-            }
-        }
-
-        private async void ExecuteCommand(string command)
-        {
-            int spaceIndex = command.IndexOf(" ");
-            string commandName,arg0 = "";
-            if (spaceIndex != -1)
-            {
-                commandName = command.Substring(0, spaceIndex);
-                arg0 = command.Substring(spaceIndex, command.Length - spaceIndex).Trim();
-            }
-            else commandName = command;
-            switch (commandName)
-            {
-                case "jumper":
-                    {
-                        jumper = !jumper;
-                        DrawNotification("jumper " + (jumper ? "on" : "off"));
-                        break;
-                    }
-                case "car":
-                    {
-                        Vector3 vehPos = Game.PlayerPed.GetOffsetPosition(new Vector3(0f, 8f, 0.5f));
-                        string veh = arg0;
-                        uint vehicleHash = (uint)API.GetHashKey(veh);
-                        API.RequestModel(vehicleHash);
-
-                        int i = 0;
-                        while (++i < 2000 && !API.HasModelLoaded(vehicleHash))
-                            await Delay(1);
-                        if (i < 2000)
-                        {
-                            API.CreateVehicle(vehicleHash, vehPos.X, vehPos.Y, vehPos.Z, API.GetEntityHeading(API.PlayerPedId()) + 90, true, false);
-                            DrawNotification("Created car");
-                        }
-                        else DrawNotification("Could not load car");
-
-                        break;
-                    }
-                case "wanted":
-                    {
-                        int level = int.Parse(arg0);
-                        Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Game.Player.Handle, level, false);
-                        break;
-                    }
-                default:
-                    DrawNotification($"command {commandName} not existing yet");
-                    break;
-            }
-        }
-
+        #endregion
+        #region Notifications
         private void DrawNotification(string text, bool blink = true)
         {
             API.SetNotificationTextEntry("STRING");
             API.AddTextComponentString(text);
             API.DrawNotification(blink, false);
         }
-        
+        private void ShowChatNotification(string text)
+        {
+            TriggerEvent("chatMessage", "", new int[] { 0, 0, 0 }, text);
+        }
+        #endregion
+        #region CommandsHandlers
+        private void ToggleJumper(int source, List<object> args, string raw)
+        {
+            jumper = !jumper;
+            ShowChatNotification("jumper " + (jumper ? "on" : "off"));
+        }
+        private async void SpawnCar(int source, List<object> args, string raw)
+        {
+            Vector3 vehPos = Game.PlayerPed.GetOffsetPosition(new Vector3(0f, 8f, 0.5f));
+            string veh = (string)args[0];
+            uint vehicleHash = (uint)API.GetHashKey(veh);
+            API.RequestModel(vehicleHash);
+
+            int i = 0;
+            while (++i < 2000 && !API.HasModelLoaded(vehicleHash))
+                await Delay(1);
+            if (i < 2000)
+            {
+                API.CreateVehicle(vehicleHash, vehPos.X, vehPos.Y, vehPos.Z, API.GetEntityHeading(API.PlayerPedId()) + 90, true, false);
+                ShowChatNotification("Created car");
+            }
+            else ShowChatNotification("Could not load car");
+
+        }
+        private void SetWantedLevel(int source, List<object> args, string raw)
+        {
+            try
+            {
+                int level = (int)args[0];
+                ShowChatNotification($"level {level}");
+                Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Game.Player.Handle, level, false);
+
+            }
+            catch (Exception e)
+            {
+                ShowChatNotification($"Exception in wanted: {e}");
+            }
+        }
+        #endregion
     }
 }
